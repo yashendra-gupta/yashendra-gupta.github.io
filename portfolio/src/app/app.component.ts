a7fb482e1b34dfe645ee09d5b6c1f0bb090b4923
Yashendra Gupta <yashendragupta2704@gmail.com>
2023-07-09 03:05:22 +0530

    0.0.12 upgrade to yg-vishva v0.0.8 - Extracted out portfolio 3d data from code

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { VishvaData } from '@yashendra-gupta/yg-vishva';
 
 import '@yashendra-gupta/yg-vishva/yg-vishva.js';
 
@@ -9,4 +11,15 @@ import '@yashendra-gupta/yg-vishva/yg-vishva.js';
 })
 export class AppComponent {
   title = 'Yashendra Gupta Portfolio';
+  vishvaData: VishvaData;
+  showYgVishva: boolean = false;
+
+  constructor(private http: HttpClient) {
+    this.http.get<any>(
+      "https://raw.githubusercontent.com/yashendra-gupta/yashendra-gupta.github.io/assets/assets/data/portfolio-3d.json")
+      .subscribe((data: any) =>  {
+        this.vishvaData = data;
+        this.showYgVishva = true;
+      });
+  }
 }
